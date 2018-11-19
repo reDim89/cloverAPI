e3213fe9f56e69645fab745608ff2fb8f7c48613
reDim89 <idmitat@gmail.com>
2018-11-19 22:01:21 +0300

    Refactored API structure and /bars method

--- clover/app.py
+++ clover/app.py
@@ -1,12 +1,7 @@
 import falcon
 
-from .addUser import addUser
-from .bars import Bars
+from .api.bars import Bars
 
 api = application = falcon.API()
 
-addUser = addUser()
-api.add_route('/users', addUser)
-
-bars = Bars()
-api.add_route('/bars', bars)
+api.add_route('/bars', Bars())
